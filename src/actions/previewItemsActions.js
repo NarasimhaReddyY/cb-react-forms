58d574b6d9f019411413ee4e6228428ae2918e51
sachinmjadhav <sachin0321@gmail.com>
2019-05-27 14:11:28 +0530

    added all the drag/drop features to the Preview component

--- src/actions/previewItemsActions.js
+++ src/actions/previewItemsActions.js
@@ -1,5 +1,12 @@
-import { ADD_ITEM_TO_PREVIEW, REMOVE_ITEM_FROM_PREVIEW } from './types';
+import uuid from "uuid/v4";
+import { ADD_ITEM_TO_PREVIEW, REMOVE_ITEM_FROM_PREVIEW, MOVE_ITEM_IN_PREVIEW } from "./types";
 
-export const addItem = item => ({ type: ADD_ITEM_TO_PREVIEW, payload: item });
+export const addItem = element => {
+	const id = uuid();
+	const item = { id, element };
+	return { type: ADD_ITEM_TO_PREVIEW, payload: item };
+};
 
-export const removeItem = id => ({ type: REMOVE_ITEM_FROM_PREVIEW, payload: id })
+export const removeItem = id => ({ type: REMOVE_ITEM_FROM_PREVIEW, payload: id });
+
+export const moveItem = (dragIndex, hoverIndex) => ({ type: MOVE_ITEM_IN_PREVIEW, payload: { dragIndex, hoverIndex } });
--- src/components/FormBuilder/Preview/index.js
+++ src/components/FormBuilder/Preview/index.js
@@ -1,57 +1,48 @@
-import React, { Component } from 'react';
-import { DropTarget } from 'react-dnd';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { removeItem } from '../../../actions/previewItemsActions';
+import React, { Component } from "react";
+import { DropTarget } from "react-dnd";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { removeItem, moveItem } from "../../../actions/previewItemsActions";
+import PreviewItems from "../PreviewItems";
 
+// DropTarget parameters
 const type = props => {
-  return "items"
-}
+	return "items";
+};
 
 const collect = (connect, monitor) => {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    hovered: monitor.isOver(),
-    item: monitor.getItem()
-  }
-}
+	return {
+		connectDropTarget: connect.dropTarget(),
+		hovered: monitor.isOver(),
+		item: monitor.getItem()
+	};
+};
 
 class Preview extends Component {
-  render() {
-    const { connectDropTarget, hovered, item, previewItems } = this.props;
-    
-    const border = hovered ? '1px solid green' : '1px solid white'
-    
-    return connectDropTarget(
-      <div style={{ height: '100%'}}>
-        <h3 className="text-center">Preview</h3>
-        <div className="jumbotron h-100" style={{ border }}>
-          {
-            previewItems.length === 0 && 
-            (
-              <h3 className="list-group-item bg-light text-center text-muted">
-                Select / Drop an item from Toolbox
-              </h3>
-            )
-          }
+	render() {
+		const { connectDropTarget, hovered, moveItem, previewItems } = this.props;
+		const border = hovered ? "1px solid green" : "1px solid white";
 
-          {
-            previewItems.length > 0 && (
-              previewItems.map((item, i) => (
-                <div className="list-group-item mb-1" key={item + i}>
-                  <span onClick={() => this.props.removeItem(i)}><i className="fa fa-times"></i> </span>
-                  {item}
-                </div>
-              ))
-            )
-          }
-        </div>
-      </div>
-    )
-  }
+		return connectDropTarget(
+			<div style={{ height: "100%" }}>
+				<h3 className="text-center">Preview</h3>
+				<div className="jumbotron h-100" style={{ border }}>
+					{previewItems.length === 0 && <h3 className="list-group-item bg-light text-center text-muted">Select / Drop an item from Toolbox</h3>}
+
+					{previewItems.length > 0 &&
+						previewItems.map((item, i) => (
+							<PreviewItems key={item.id} index={i} id={item.id} removeItem={this.props.removeItem} item={item.element} moveItem={moveItem} />
+						))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default compose(
-  connect(state => ({ previewItems: state.previewItems }), { removeItem }),
-  DropTarget(type, {}, collect)
-)(Preview)
+	connect(
+		state => ({ previewItems: state.previewItems }),
+		{ removeItem, moveItem }
+	),
+	DropTarget(type, {}, collect)
+)(Preview);
